da1d5887a4d988f6472779a002453406ecd9ec60
iAmBarbara <iAmBarbara@users.noreply.github.com>
2022-12-20 19:14:06 +0100

    Add function for toggled menu, add function for smooth scrolling

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,21 @@
 console.log("Hi and welcome to my Portfolio")
+
+function menuToggle() {
+    var x = document.getElementById('myNavtoggle');
+    if (x.className === 'navtoggle') {
+      x.className += ' responsive';
+    } else {
+      x.className = 'navtoggle';
+    }
+}
+
+$('a[href^="#"]').on('click',function(e) {
+    e.preventDefault();
+    var target = this.hash;
+    var $target = $(target);
+    $('html, body').stop().animate({
+     'scrollTop': $target.offset().top
+    }, 800, 'swing', function () {
+     window.location.hash = target;
+    });
+   });
